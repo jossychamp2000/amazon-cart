da9d3b80e059991787715be8eef6e0951ca986c9
janejane <janetjanedchic@yahoo.com>
2021-02-03 22:18:49 -0500

    Add:Header component

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="App">
+            <div className="App-header">
+                <h1>Amazon Cart</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
