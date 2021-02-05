0b45655d957ec4e820c4134de9feefac2b434fd0
janejane <janetjanedchic@yahoo.com>
2021-02-04 19:29:45 -0500

    ADD:Cartitems compnents, CartTotal component CHNG: move files to components folder

---
+++ src/Cartitems.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '.components//CartItems.css'
+
+function Cartitems() {
+    return (
+        <div className="CartItems">
+
+            <h1>Shopping Cart</h1>
+
+        </div>
+    )
+}
+
+export default Cartitems
