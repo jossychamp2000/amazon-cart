4116c046bcb8e0099500e9a5a377c89fe3b4aa66
janejane <janetjanedchic@yahoo.com>
2021-02-07 20:40:02 -0500

    ADD:change item quantity

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './CartItem.css'
 
 
-function CartItem({ item }) {
+function CartItem({ index, item, changeItemQuantity }) {
 
     return (
         <div className="CartItem">
@@ -19,10 +19,15 @@ function CartItem({ item }) {
                 </div>
                 <div className="item-actions">
                     <div className="item-quantity">
-                        <select value={item.quantity}>
+                        <select
+                            onChange={(e) => changeItemQuantity(e, index)}
+                            value={item.quantity}>
                             <option value="1">Qty: 1</option>
                             <option value="2">Qty: 2</option>
                             <option value="3">Qty: 3</option>
+                            <option value="4">Qty: 4</option>
+                            <option value="5">Qty: 5</option>
+                            <option value="6">Qty: 6</option>
 
                         </select>
 
--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -2,7 +2,16 @@ import React from 'react';
 import './CartItems.css'
 import CartItem from './CartItem'
 
-function CartItems({ items }) {
+function CartItems({ items, setCartItems }) {
+
+    const changeItemQuantity = (e, index) => {
+
+        const newItems = [...items]
+        items[index].quantity = e.target.value
+        setCartItems(newItems);
+
+
+    }
 
     return (
         <div className="CartItems">
@@ -12,8 +21,10 @@ function CartItems({ items }) {
             <div className="CartItems-items">
                 {items.map((item, index) =>
                     <CartItem
+                        index={index}
                         item={item}
                         key={index}
+                        changeItemQuantity={changeItemQuantity}
                     />
                 )}
 
